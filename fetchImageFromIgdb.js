40f6f632b7a071ea31e5aaf115bb0dfb1d0bdfad
javascriptsucks <evix1988@live.cn>
2023-06-11 15:47:23 -0600

    increated more fields, create json file after fetch

--- fetchImageFromIgdb.js
+++ fetchImageFromIgdb.js
@@ -1,34 +1,30 @@
 /* eslint-disable */
 require("dotenv").config();
 
+const fs = require("fs");
 const axios = require("axios");
+const path = require("path");
 
-const API_KEY = process.env.CLIENT_ID;
-const token = process.env.IGDB_TOKEN;
+const startingDate = new Date(2016, 0, 1).getTime();
+const headers = {
+	"Client-ID": process.env.CLIENT_ID,
+	Authorization: `Bearer ${process.env.IGDB_TOKEN}`,
+};
 
 const fetchGames = async () => {
 	const url = "https://api.igdb.com/v4/games";
-	const headers = {
-		"Client-ID": API_KEY,
-		Authorization: `Bearer ${token}`,
-	};
 
 	const response = await axios.post(
 		url,
-		`fields name, genres, tags, platforms, themes, first_release_date, summary, total_rating, total_rating_count, cover; where cover != null; limit: 100;`,
+		`fields name, slug, genres, tags, platforms, themes, first_release_date, summary, total_rating, total_rating_count, cover, screenshots; where screenshots != null & cover != null & total_rating_count > 220 & first_release_date > ${startingDate}; limit: 20;`,
 		{ headers }
 	);
 
-	console.log("response.data", response.data);
 	return response.data;
 };
 
 async function getCoverImage(coverId) {
 	const url = "https://api.igdb.com/v4/covers";
-	const headers = {
-		"Client-ID": API_KEY,
-		Authorization: `Bearer ${token}`,
-	};
 
 	const response = await axios.post(url, `fields url; where id = ${coverId};`, {
 		headers,
@@ -36,8 +32,91 @@ async function getCoverImage(coverId) {
 
 	console.log("return of response in fetchImageFromIgdb.js", response.data);
 
-	return response.data[0].url;
+	const coverUrl = response.data[0].url.replace("t_thumb", "t_cover_big");
+
+	return coverUrl;
 }
+
+const getGameScreenshots = async (screenshotsArr) => {
+	const url = "https://api.igdb.com/v4/screenshots";
+	const screenshotsReturnData = [];
+	for (const screenshotID of screenshotsArr) {
+		const res = await axios.post(
+			url,
+			`fields url; where id = ${screenshotID};`,
+			{
+				headers,
+			}
+		);
+
+		const screenshotUrl = res.data[0].url.replace(
+			"t_thumb",
+			"t_screenshot_big"
+		);
+		screenshotsReturnData.push(screenshotUrl);
+		await new Promise((resolve) => setTimeout(resolve, 1));
+	}
+	return screenshotsReturnData;
+};
+
+const getGameGenres = async (genresArr) => {
+	const url = "https://api.igdb.com/v4/genres";
+	const genresReturnData = [];
+
+	for (const genreID of genresArr) {
+		const res = await axios.post(url, `fields name; where id = ${genreID};`, {
+			headers,
+		});
+
+		genresReturnData.push(res.data[0].name);
+		await new Promise((resolve) => setTimeout(resolve, 1));
+	}
+
+	return genresReturnData;
+};
+
+const getGamePlatforms = async (platformsArr) => {
+	const url = "https://api.igdb.com/v4/platforms";
+	const platformsReturnData = [];
+	for (const platformID of platformsArr) {
+		const res = await axios.post(
+			url,
+			`fields name; where id = ${platformID};`,
+			{
+				headers,
+			}
+		);
+		platformsReturnData.push(res.data[0].name);
+		await new Promise((resolve) => setTimeout(resolve, 1));
+	}
+	return platformsReturnData;
+};
+
+const getGameThemes = async (themesArr) => {
+	const url = "https://api.igdb.com/v4/themes";
+	const themesReturnData = [];
+	for (const themeID of themesArr) {
+		const res = await axios.post(url, `fields name; where id = ${themeID};`, {
+			headers,
+		});
+		themesReturnData.push(res.data[0].name);
+		await new Promise((resolve) => setTimeout(resolve, 1));
+	}
+	return themesReturnData;
+};
+
+const getGameTags = async (tagsArr) => {
+	const url = "https://api.igdb.com/v4/tags";
+	const tagsReturnData = [];
+	for (const tagID of tagsArr) {
+		const res = await axios.post(url, `fields name; where id = ${tagID};`, {
+			headers,
+		});
+		tagsReturnData.push(res.data[0].name);
+		await new Promise((resolve) => setTimeout(resolve, 1));
+	}
+	return tagsReturnData;
+};
 
 const generateGameObjects = async () => {
 	const gameData = await fetchGames();
@@ -45,14 +124,41 @@ const generateGameObjects = async () => {
 	const games = [];
 	for (const game of gameData) {
 		const coverUrl = game.cover ? await getCoverImage(game.cover) : null;
+
+		// const tags = game.tags ? await getGameTags(game.tags) : null;
+		const platforms = game.platforms
+			? await getGamePlatforms(game.platforms)
+			: null;
+
+		const themes = game.themes ? await getGameThemes(game.themes) : null;
+		const genres = game.genres ? await getGameGenres(game.genres) : null;
+		const screenshots = game.screenshots
+			? await getGameScreenshots(game.screenshots)
+			: null;
 		games.push({
+			...game,
+			genres,
 			id: game.id,
 			name: game.name,
-			coverUrl: coverUrl,
+			cover: coverUrl,
+			platforms,
+			themes,
+			screenshots,
 		});
 
-		await new Promise((resolve) => setTimeout(resolve, 251));
+		await new Promise((resolve) => setTimeout(resolve, 10));
 	}
+
+	const jsonData = JSON.stringify(games, null, 2);
+	const filePath = path.join(__dirname, "games.json");
+
+	fs.writeFile(filePath, jsonData, (err) => {
+		if (err) {
+			console.error("Error writing JSON file:", err);
+		} else {
+			console.log("JSON file created successfully.");
+		}
+	});
 
 	return games;
 };
